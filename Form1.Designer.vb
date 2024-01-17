f59d1f8ee2af34a06e9806e06d43301a0eb03783
Academy-User <Academy-User@DESKTOP-1L3B0TP>
2024-01-17 12:18:00 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Silver
+        Label1.Location = New Point(130, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(66, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter a"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Silver
+        Label2.Location = New Point(128, 119)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(68, 25)
+        Label2.TabIndex = 0
+        Label2.Text = "Enter b"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Cyan
+        Button1.Location = New Point(21, 193)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(256, 34)
+        Button1.TabIndex = 1
+        Button1.Text = "C(Hypothenus) is equal to"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(314, 51)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(187, 31)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(314, 119)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(187, 31)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(314, 196)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(187, 31)
+        TextBox3.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        BackgroundImage = My.Resources.Resources.WhatsApp_Image_2024_01_17_at_11_50_36
+        ClientSize = New Size(749, 697)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Double
+
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+
+        c = (a ^ 2 + b ^ 2) ^ 0.5
+
+        TextBox3.Text = c
+    End Sub
+End Class
